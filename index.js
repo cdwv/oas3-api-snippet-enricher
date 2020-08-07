57e79fce1ff6427d1b58b0cc4cd7ec32ed87266d
Rohit Gohri <work@rohit.page>
2020-08-07 22:37:32 +0530

    skip custom methods

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function enrichSchema(schema){
 	for(var path in schema.paths){
 			
 		for(var method in schema.paths[path]){
+			if (method.indexOf('x-') === 0) continue;
 			var generatedCode = OpenAPISnippet.getEndpointSnippets(schema, path, method, targets);
 			schema.paths[path][method]["x-codeSamples"] = [];
 			for(var snippetIdx in generatedCode.snippets){
